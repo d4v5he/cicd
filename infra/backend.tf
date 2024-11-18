7feca2ece59932daee9b0df30e63ce0e32326988
d4v5he <d4v5he@gmail.com>
2024-11-18 23:49:57 +0200

    REMOVE ALL

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "davshe-technion-cicd-13579"
-    key            = "terraform/state/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}

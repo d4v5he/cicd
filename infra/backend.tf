a99de9c02ad6a27ee6bd06b924d5e6757e6faffe
yafit haftzadi <yafit@classiq.io>
2024-11-20 14:50:05 +0200

    2edas

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "davshe-technion-cicd-13579"
+    bucket         = "yafit89-technion-cicd-13579"
     key            = "terraform/state/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
